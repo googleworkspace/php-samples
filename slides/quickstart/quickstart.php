7e21aea963dfccf25b4b65385ddf75162fd10f79
Brent Shaffer <betterbrent@google.com>
2018-03-19 15:42:31 -0700

    fixes slides cs

--- slides/quickstart/quickstart.php
+++ slides/quickstart/quickstart.php
@@ -19,42 +19,43 @@
  * Returns an authorized API client.
  * @return Google_Client the authorized client object
  */
-function getClient() {
-  $client = new Google_Client();
-  $client->setApplicationName('Google Slides API PHP Quickstart');
-  $client->setScopes(Google_Service_Slides::PRESENTATIONS_READONLY);
-  $client->setAuthConfig('client_secret.json');
-  $client->setAccessType('offline');
+function getClient()
+{
+    $client = new Google_Client();
+    $client->setApplicationName('Google Slides API PHP Quickstart');
+    $client->setScopes(Google_Service_Slides::PRESENTATIONS_READONLY);
+    $client->setAuthConfig('client_secret.json');
+    $client->setAccessType('offline');
 
-  // Load previously authorized credentials from a file.
-  $credentialsPath = expandHomeDirectory('credentials.json');
-  if (file_exists($credentialsPath)) {
-    $accessToken = json_decode(file_get_contents($credentialsPath), true);
-  } else {
-    // Request authorization from the user.
-    $authUrl = $client->createAuthUrl();
-    printf("Open the following link in your browser:\n%s\n", $authUrl);
-    print 'Enter verification code: ';
-    $authCode = trim(fgets(STDIN));
+    // Load previously authorized credentials from a file.
+    $credentialsPath = expandHomeDirectory('credentials.json');
+    if (file_exists($credentialsPath)) {
+        $accessToken = json_decode(file_get_contents($credentialsPath), true);
+    } else {
+        // Request authorization from the user.
+        $authUrl = $client->createAuthUrl();
+        printf("Open the following link in your browser:\n%s\n", $authUrl);
+        print 'Enter verification code: ';
+        $authCode = trim(fgets(STDIN));
 
-    // Exchange authorization code for an access token.
-    $accessToken = $client->fetchAccessTokenWithAuthCode($authCode);
+        // Exchange authorization code for an access token.
+        $accessToken = $client->fetchAccessTokenWithAuthCode($authCode);
 
-    // Store the credentials to disk.
-    if (!file_exists(dirname($credentialsPath))) {
-      mkdir(dirname($credentialsPath), 0700, true);
+        // Store the credentials to disk.
+        if (!file_exists(dirname($credentialsPath))) {
+            mkdir(dirname($credentialsPath), 0700, true);
+        }
+        file_put_contents($credentialsPath, json_encode($accessToken));
+        printf("Credentials saved to %s\n", $credentialsPath);
     }
-    file_put_contents($credentialsPath, json_encode($accessToken));
-    printf("Credentials saved to %s\n", $credentialsPath);
-  }
-  $client->setAccessToken($accessToken);
+    $client->setAccessToken($accessToken);
 
-  // Refresh the token if it's expired.
-  if ($client->isAccessTokenExpired()) {
-    $client->fetchAccessTokenWithRefreshToken($client->getRefreshToken());
-    file_put_contents($credentialsPath, json_encode($client->getAccessToken()));
-  }
-  return $client;
+    // Refresh the token if it's expired.
+    if ($client->isAccessTokenExpired()) {
+        $client->fetchAccessTokenWithRefreshToken($client->getRefreshToken());
+        file_put_contents($credentialsPath, json_encode($client->getAccessToken()));
+    }
+    return $client;
 }
 
 /**
@@ -62,12 +63,13 @@ function getClient() {
  * @param string $path the path to expand.
  * @return string the expanded path.
  */
-function expandHomeDirectory($path) {
-  $homeDirectory = getenv('HOME');
-  if (empty($homeDirectory)) {
-    $homeDirectory = getenv('HOMEDRIVE') . getenv('HOMEPATH');
-  }
-  return str_replace('~', realpath($homeDirectory), $path);
+function expandHomeDirectory($path)
+{
+    $homeDirectory = getenv('HOME');
+    if (empty($homeDirectory)) {
+        $homeDirectory = getenv('HOMEDRIVE') . getenv('HOMEPATH');
+    }
+    return str_replace('~', realpath($homeDirectory), $path);
 }
 
 // Get the API client and construct the service object.
@@ -82,7 +84,7 @@ $slides = $presentation->getSlides();
 
 printf("The presentation contains %s slides:\n", count($slides));
 foreach ($slides as $i => $slide) {
-  // Print columns A and E, which correspond to indices 0 and 4.
-  printf("- Slide #%s contains %s elements.\n", $i + 1,
+    // Print columns A and E, which correspond to indices 0 and 4.
+    printf("- Slide #%s contains %s elements.\n", $i + 1,
       count($slide->getPageElements()));
 }
