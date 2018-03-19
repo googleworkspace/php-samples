ad474bf69916db8e8a8f8f3fb92f152b5e3956c5
Brent Shaffer <betterbrent@google.com>
2018-03-19 19:43:28 -0400

    Adds CS check to travis, fixes CS (#2)
    * adds php cs fixer
    * fixes calendar cs
    * fixes apps-script cs
    * fixes drive cs
    * fixes sheets cs
    * fixes slides cs

--- apps-script/quickstart/quickstart.php
+++ apps-script/quickstart/quickstart.php
@@ -20,42 +20,43 @@ require_once __DIR__ . '/vendor/autoload.php';
  * Returns an authorized API client.
  * @return Google_Client the authorized client object
  */
-function getClient() {
-  $client = new Google_Client();
-  $client->setApplicationName('Google Apps Script API PHP Quickstart');
-  $client->setScopes("https://www.googleapis.com/auth/script.projects");
-  $client->setAuthConfig('client_secret.json');
-  $client->setAccessType('offline');
+function getClient()
+{
+    $client = new Google_Client();
+    $client->setApplicationName('Google Apps Script API PHP Quickstart');
+    $client->setScopes("https://www.googleapis.com/auth/script.projects");
+    $client->setAuthConfig('client_secret.json');
+    $client->setAccessType('offline');
 
-  // Load previously authorized credentials from a file.
-  $credentialsPath = expandHomeDirectory('credentials.json');
-  if (file_exists($credentialsPath)) {
-    $accessToken = json_decode(file_get_contents($credentialsPath), true);
-  } else {
-    // Request authorization from the user.
-    $authUrl = $client->createAuthUrl();
-    printf("Open the following link in your browser:\n%s\n", $authUrl);
-    print 'Enter verification code: ';
-    $authCode = trim(fgets(STDIN));
+    // Load previously authorized credentials from a file.
+    $credentialsPath = expandHomeDirectory('credentials.json');
+    if (file_exists($credentialsPath)) {
+        $accessToken = json_decode(file_get_contents($credentialsPath), true);
+    } else {
+        // Request authorization from the user.
+        $authUrl = $client->createAuthUrl();
+        printf("Open the following link in your browser:\n%s\n", $authUrl);
+        print 'Enter verification code: ';
+        $authCode = trim(fgets(STDIN));
 
-    // Exchange authorization code for an access token.
-    $accessToken = $client->fetchAccessTokenWithAuthCode($authCode);
+        // Exchange authorization code for an access token.
+        $accessToken = $client->fetchAccessTokenWithAuthCode($authCode);
 
-    // Store the credentials to disk.
-    if(!file_exists(dirname($credentialsPath))) {
-      mkdir(dirname($credentialsPath), 0700, true);
+        // Store the credentials to disk.
+        if (!file_exists(dirname($credentialsPath))) {
+            mkdir(dirname($credentialsPath), 0700, true);
+        }
+        file_put_contents($credentialsPath, json_encode($accessToken));
+        printf("Credentials saved to %s\n", $credentialsPath);
     }
-    file_put_contents($credentialsPath, json_encode($accessToken));
-    printf("Credentials saved to %s\n", $credentialsPath);
-  }
-  $client->setAccessToken($accessToken);
+    $client->setAccessToken($accessToken);
 
-  // Refresh the token if it's expired.
-  if ($client->isAccessTokenExpired()) {
-    $client->fetchAccessTokenWithRefreshToken($client->getRefreshToken());
-    file_put_contents($credentialsPath, json_encode($client->getAccessToken()));
-  }
-  return $client;
+    // Refresh the token if it's expired.
+    if ($client->isAccessTokenExpired()) {
+        $client->fetchAccessTokenWithRefreshToken($client->getRefreshToken());
+        file_put_contents($credentialsPath, json_encode($client->getAccessToken()));
+    }
+    return $client;
 }
 
 /**
@@ -63,12 +64,13 @@ function getClient() {
  * @param string $path the path to expand.
  * @return string the expanded path.
  */
-function expandHomeDirectory($path) {
-  $homeDirectory = getenv('HOME');
-  if (empty($homeDirectory)) {
-    $homeDirectory = getenv('HOMEDRIVE') . getenv('HOMEPATH');
-  }
-  return str_replace('~', realpath($homeDirectory), $path);
+function expandHomeDirectory($path)
+{
+    $homeDirectory = getenv('HOME');
+    if (empty($homeDirectory)) {
+        $homeDirectory = getenv('HOMEDRIVE') . getenv('HOMEPATH');
+    }
+    return str_replace('~', realpath($homeDirectory), $path);
 }
 
 /**
--- calendar/quickstart/quickstart.php
+++ calendar/quickstart/quickstart.php
@@ -20,42 +20,43 @@ require_once __DIR__ . '/vendor/autoload.php';
  * Returns an authorized API client.
  * @return Google_Client the authorized client object
  */
-function getClient() {
-  $client = new Google_Client();
-  $client->setApplicationName('Google Calendar API PHP Quickstart');
-  $client->setScopes(Google_Service_Calendar::CALENDAR_READONLY);
-  $client->setAuthConfig('client_secret.json');
-  $client->setAccessType('offline');
+function getClient()
+{
+    $client = new Google_Client();
+    $client->setApplicationName('Google Calendar API PHP Quickstart');
+    $client->setScopes(Google_Service_Calendar::CALENDAR_READONLY);
+    $client->setAuthConfig('client_secret.json');
+    $client->setAccessType('offline');
 
-  // Load previously authorized credentials from a file.
-  $credentialsPath = expandHomeDirectory('credentials.json');
-  if (file_exists($credentialsPath)) {
-    $accessToken = json_decode(file_get_contents($credentialsPath), true);
-  } else {
-    // Request authorization from the user.
-    $authUrl = $client->createAuthUrl();
-    printf("Open the following link in your browser:\n%s\n", $authUrl);
-    print 'Enter verification code: ';
-    $authCode = trim(fgets(STDIN));
+    // Load previously authorized credentials from a file.
+    $credentialsPath = expandHomeDirectory('credentials.json');
+    if (file_exists($credentialsPath)) {
+        $accessToken = json_decode(file_get_contents($credentialsPath), true);
+    } else {
+        // Request authorization from the user.
+        $authUrl = $client->createAuthUrl();
+        printf("Open the following link in your browser:\n%s\n", $authUrl);
+        print 'Enter verification code: ';
+        $authCode = trim(fgets(STDIN));
 
-    // Exchange authorization code for an access token.
-    $accessToken = $client->fetchAccessTokenWithAuthCode($authCode);
+        // Exchange authorization code for an access token.
+        $accessToken = $client->fetchAccessTokenWithAuthCode($authCode);
 
-    // Store the credentials to disk.
-    if (!file_exists(dirname($credentialsPath))) {
-      mkdir(dirname($credentialsPath), 0700, true);
+        // Store the credentials to disk.
+        if (!file_exists(dirname($credentialsPath))) {
+            mkdir(dirname($credentialsPath), 0700, true);
+        }
+        file_put_contents($credentialsPath, json_encode($accessToken));
+        printf("Credentials saved to %s\n", $credentialsPath);
     }
-    file_put_contents($credentialsPath, json_encode($accessToken));
-    printf("Credentials saved to %s\n", $credentialsPath);
-  }
-  $client->setAccessToken($accessToken);
+    $client->setAccessToken($accessToken);
 
-  // Refresh the token if it's expired.
-  if ($client->isAccessTokenExpired()) {
-    $client->fetchAccessTokenWithRefreshToken($client->getRefreshToken());
-    file_put_contents($credentialsPath, json_encode($client->getAccessToken()));
-  }
-  return $client;
+    // Refresh the token if it's expired.
+    if ($client->isAccessTokenExpired()) {
+        $client->fetchAccessTokenWithRefreshToken($client->getRefreshToken());
+        file_put_contents($credentialsPath, json_encode($client->getAccessToken()));
+    }
+    return $client;
 }
 
 /**
@@ -63,12 +64,13 @@ function getClient() {
  * @param string $path the path to expand.
  * @return string the expanded path.
  */
-function expandHomeDirectory($path) {
-  $homeDirectory = getenv('HOME');
-  if (empty($homeDirectory)) {
-    $homeDirectory = getenv('HOMEDRIVE') . getenv('HOMEPATH');
-  }
-  return str_replace('~', realpath($homeDirectory), $path);
+function expandHomeDirectory($path)
+{
+    $homeDirectory = getenv('HOME');
+    if (empty($homeDirectory)) {
+        $homeDirectory = getenv('HOMEDRIVE') . getenv('HOMEPATH');
+    }
+    return str_replace('~', realpath($homeDirectory), $path);
 }
 
 // Get the API client and construct the service object.
@@ -80,20 +82,20 @@ $calendarId = 'primary';
 $optParams = array(
   'maxResults' => 10,
   'orderBy' => 'startTime',
-  'singleEvents' => TRUE,
+  'singleEvents' => true,
   'timeMin' => date('c'),
 );
 $results = $service->events->listEvents($calendarId, $optParams);
 
 if (empty($results->getItems())) {
-  print "No upcoming events found.\n";
+    print "No upcoming events found.\n";
 } else {
-  print "Upcoming events:\n";
-  foreach ($results->getItems() as $event) {
-    $start = $event->start->dateTime;
-    if (empty($start)) {
-      $start = $event->start->date;
+    print "Upcoming events:\n";
+    foreach ($results->getItems() as $event) {
+        $start = $event->start->dateTime;
+        if (empty($start)) {
+            $start = $event->start->date;
+        }
+        printf("%s (%s)\n", $event->getSummary(), $start);
     }
-    printf("%s (%s)\n", $event->getSummary(), $start);
-  }
 }
