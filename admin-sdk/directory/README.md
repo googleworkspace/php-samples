e85f1292f44c2fc086d4cc995c77d4f0e8557026
Grant Timmerman <timmerman+devrel@google.com>
2018-04-10 20:43:55 -0700

    Add missing directory quickstart

---
+++ admin-sdk/directory/README.md
@@ -0,0 +1,25 @@
+# Admin SDK - Directory API Quickstart
+
+Complete the steps described in the [Admin SDK - Directory API Quickstart](https://developers.google.com/admin-sdk/directory/v1/quickstart/php), and in about five minutes you'll have a simple PHP command-line application that makes requests to the Admin SDK.
+
+## Set up
+
+### Install Composer Globally
+
+Before running this quickstart, be sure you have [Composer installed globally](https://getcomposer.org/doc/00-intro.md#globally).
+
+```sh
+composer install
+```
+
+### Download Service Account Credentials
+
+- Go to [APIs and Services](https://pantheon.corp.google.com/apis/credentials) and click "Create Credentials".
+  Select "Service Account Key" using the JSON key type, and select "Create".
+  Once downloaded, rename the credentials file to `~/credentials.json`
+
+## Run
+
+```sh
+php quickstart.php
+```
---
+++ admin-sdk/directory/quickstart.php
@@ -0,0 +1,96 @@
+<?php
+/**
+ * Copyright 2018 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+// [START admin_sdk_directory_quickstart]
+/**
+ * Returns an authorized API client.
+ * @return Google_Client the authorized client object
+ */
+function getClient()
+{
+    $client = new Google_Client();
+    $client->setApplicationName('G Suite Directory API PHP Quickstart');
+    $client->setScopes(Google_Service_Directory::ADMIN_DIRECTORY_USER_READONLY);
+    $client->setAuthConfig('client_secret.json');
+    $client->setAccessType('offline');
+
+    // Load previously authorized credentials from a file.
+    $credentialsPath = expandHomeDirectory('credentials.json');
+    if (file_exists($credentialsPath)) {
+        $accessToken = json_decode(file_get_contents($credentialsPath), true);
+    } else {
+        // Request authorization from the user.
+        $authUrl = $client->createAuthUrl();
+        printf("Open the following link in your browser:\n%s\n", $authUrl);
+        print 'Enter verification code: ';
+        $authCode = trim(fgets(STDIN));
+
+        // Exchange authorization code for an access token.
+        $accessToken = $client->fetchAccessTokenWithAuthCode($authCode);
+
+        // Store the credentials to disk.
+        if (!file_exists(dirname($credentialsPath))) {
+            mkdir(dirname($credentialsPath), 0700, true);
+        }
+        file_put_contents($credentialsPath, json_encode($accessToken));
+        printf("Credentials saved to %s\n", $credentialsPath);
+    }
+    $client->setAccessToken($accessToken);
+
+    // Refresh the token if it's expired.
+    if ($client->isAccessTokenExpired()) {
+        $client->fetchAccessTokenWithRefreshToken($client->getRefreshToken());
+        file_put_contents($credentialsPath, json_encode($client->getAccessToken()));
+    }
+    return $client;
+}
+
+/**
+ * Expands the home directory alias '~' to the full path.
+ * @param string $path the path to expand.
+ * @return string the expanded path.
+ */
+function expandHomeDirectory($path)
+{
+    $homeDirectory = getenv('HOME');
+    if (empty($homeDirectory)) {
+        $homeDirectory = getenv('HOMEDRIVE') . getenv('HOMEPATH');
+    }
+    return str_replace('~', realpath($homeDirectory), $path);
+}
+
+// Get the API client and construct the service object.
+$client = getClient();
+$service = new Google_Service_Directory($client);
+
+// Print the first 10 users in the domain.
+$optParams = array(
+  'customer' => 'my_customer',
+  'maxResults' => 10,
+  'orderBy' => 'email',
+);
+$results = $service->users->listUsers($optParams);
+
+if (count($results->getUsers()) == 0) {
+  print "No users found.\n";
+} else {
+  print "Users:\n";
+  foreach ($results->getUsers() as $user) {
+    printf("%s (%s)\n", $user->getPrimaryEmail(),
+        $user->getName()->getFullName());
+  }
+}
+// [END admin_sdk_directory_quickstart]
