c7fa52a1a1dd658be5b33b74faa90fbe24ef5ed7
Eric Koleda <ekoleda@google.com>
2018-09-11 14:46:18 -0400

    Handle missing refresh tokens in the other quickstarts

--- drive/activity/quickstart.php
+++ drive/activity/quickstart.php
@@ -29,45 +29,44 @@ function getClient()
 {
     $client = new Google_Client();
     $client->setApplicationName('Google Drive Activity API Quickstart');
-    $client->setScopes(implode(' ', array(
-      Google_Service_Appsactivity::ACTIVITY,
-      Google_Service_Drive::DRIVE_METADATA_READONLY)
-    ));
+    $client->setScopes(Google_Service_Appsactivity::ACTIVITY);
     $client->setAuthConfig('credentials.json');
     $client->setAccessType('offline');
+    $client->setPrompt('select_account consent');
 
-    // Load previously authorized credentials from a file.
-    $credentialsPath = 'token.json';
-    if (file_exists($credentialsPath)) {
-        $accessToken = json_decode(file_get_contents($credentialsPath), true);
-    } else {
-        // Request authorization from the user.
-        $authUrl = $client->createAuthUrl();
-        printf("Open the following link in your browser:\n%s\n", $authUrl);
-        print 'Enter verification code: ';
-        $authCode = trim(fgets(STDIN));
-
-        // Exchange authorization code for an access token.
-        $accessToken = $client->fetchAccessTokenWithAuthCode($authCode);
-
-        // Check to see if there was an error.
-        if (array_key_exists('error', $accessToken)) {
-            throw new Exception(join(', ', $accessToken));
-        }
-
-        // Store the credentials to disk.
-        if (!file_exists(dirname($credentialsPath))) {
-            mkdir(dirname($credentialsPath), 0700, true);
-        }
-        file_put_contents($credentialsPath, json_encode($accessToken));
-        printf("Credentials saved to %s\n", $credentialsPath);
+    // Load previously authorized token from a file, if it exists.
+    $tokenPath = 'token.json';
+    if (file_exists($tokenPath)) {
+        $accessToken = json_decode(file_get_contents($tokenPath), true);
+        $client->setAccessToken($accessToken);
     }
-    $client->setAccessToken($accessToken);
 
-    // Refresh the token if it's expired.
+    // If there is no previous token or it's expired.
     if ($client->isAccessTokenExpired()) {
-        $client->fetchAccessTokenWithRefreshToken($client->getRefreshToken());
-        file_put_contents($credentialsPath, json_encode($client->getAccessToken()));
+        // Refresh the token if possible, else fetch a new one.
+        if($client->getRefreshToken()) {
+            $client->fetchAccessTokenWithRefreshToken($client->getRefreshToken());
+        } else {
+            // Request authorization from the user.
+            $authUrl = $client->createAuthUrl();
+            printf("Open the following link in your browser:\n%s\n", $authUrl);
+            print 'Enter verification code: ';
+            $authCode = trim(fgets(STDIN));
+
+            // Exchange authorization code for an access token.
+            $accessToken = $client->fetchAccessTokenWithAuthCode($authCode);
+            $client->setAccessToken($accessToken);
+
+            // Check to see if there was an error.
+            if (array_key_exists('error', $accessToken)) {
+                throw new Exception(join(', ', $accessToken));
+            }
+        }
+        // Save the token to a file.
+        if (!file_exists(dirname($tokenPath))) {
+            mkdir(dirname($tokenPath), 0700, true);
+        }
+        file_put_contents($tokenPath, json_encode($client->getAccessToken()));
     }
     return $client;
 }
