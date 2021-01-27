e71d031c800d8666d2b984b0f6114ec0e4023aa3
Steven Bazyl <sbazyl@google.com>
2021-01-27 14:25:26 -0700

    Rename gsuitedevs->googleworkspace

--- docs/samples/extract_text.php
+++ docs/samples/extract_text.php
@@ -18,7 +18,7 @@
 /**
  * For instructions on how to run the full sample:
  *
- * @see https://github.com/gsuitedevs/docs-api-samples/tree/master/php/README.md
+ * @see https://github.com/googleworkspace/docs-api-samples/tree/master/php/README.md
  */
 
 // Include Google Cloud dependendencies using Composer
