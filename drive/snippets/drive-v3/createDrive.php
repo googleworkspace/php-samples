31d1c8b578ee6e2a08d0360228b10dd77dd6b17b
anuraggoogler <aanursharm@google.com>
2022-06-16 09:31:30 +0000

    Drive-v3 samples

--- drive/snippets/drive-v3/createDrive.php
+++ drive/snippets/drive-v3/createDrive.php
@@ -1,39 +1,42 @@
 <?php
 /**
- * Copyright 2022 Google LLC
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-// [START drive_createDrive]
+* Copyright 2022 Google Inc.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+// [START createDrive]
 require_once 'vendor/autoload.php';
 use Ramsey\Uuid\Uuid;
-putenv('GOOGLE_APPLICATION_CREDENTIALS=/Users/RPSB/Documents/snippets/php_drive/drive_v3/workspace-348506-241f41f76ce5.json');
 function createDrive()
 {
-    $client = new Google\Client();
-    $client->useApplicationDefaultCredentials();
-    $client->addScope(Google\Service\Drive::DRIVE);
-    $driveService = new Google_Service_Drive($client);
-    
-    $driveMetadata = new Google_Service_Drive_Drive(array(
-            'name' => 'Project Resources'));
-    $requestId = Uuid::uuid4()->toString();
-    $drive = $driveService->drives->create($requestId, $driveMetadata, array(
-            'fields' => 'id'));
-    printf("Drive ID: %s\n", $drive->id);
-    
-   //return $drive->id;
+    try {
+        $client = new Google\Client();
+        $client->useApplicationDefaultCredentials();
+        $client->addScope(Google\Service\Drive::DRIVE);
+        $driveService = new Google_Service_Drive($client);
+        
+        $driveMetadata = new Google_Service_Drive_Drive(array(
+                'name' => 'Project Resources'));
+        $requestId = Uuid::uuid4()->toString();
+        $drive = $driveService->drives->create($requestId, $driveMetadata, array(
+                'fields' => 'id'));
+        printf("Drive ID: %s\n", $drive->id);
+        return $drive->id;
+    } catch(Exception $e)  {
+        echo "Error Message: ".$e;
+    }  
+   
 }     
-// [END drive_createDrive]
+// [END createDrive]
 createDrive();   
 ?>
--- drive/snippets/drive-v3/searchFiles.php
+++ drive/snippets/drive-v3/searchFiles.php
@@ -1,50 +1,54 @@
-<?php
+<?php 
 /**
- * Copyright 2022 Google LLC
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-// [START drive_searchFiles]
+* Copyright 2022 Google Inc.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+// [START searchFiles]
 require_once 'vendor/autoload.php';
-putenv('GOOGLE_APPLICATION_CREDENTIALS=/Users/RPSB/Documents/snippets/php_drive/drive_v3/workspace-348506-241f41f76ce5.json');
 function searchFiles()
 {
-    $client = new Google\Client();
-    $client->useApplicationDefaultCredentials();
-    $client->addScope(Google\Service\Drive::DRIVE);
-    $driveService = new Google_Service_Drive($client);
-    $files = array();
-    // [START searchFiles]
-    $pageToken = null;
-    do {
-        $response = $driveService->files->listFiles(array(
-            'q' => "mimeType='image/jpeg'",
-            'spaces' => 'drive',
-            'pageToken' => $pageToken,
-            'fields' => 'nextPageToken, files(id, name)',
-        ));
-        foreach ($response->files as $file) {
-            printf("Found file: %s (%s)\n", $file->name, $file->id);
-        }
-        // [START_EXCLUDE silent]
-        array_push($files, $response->files);
-        // [END_EXCLUDE]
-
-        $pageToken = $repsonse->pageToken;
-    } while ($pageToken != null);
-    // [END searchFiles]
-    return $files;
+    try {
+        $client = new Google\Client();
+        $client->useApplicationDefaultCredentials();
+        $client->addScope(Google\Service\Drive::DRIVE);
+        $driveService = new Google_Service_Drive($client);
+        $files = array();
+        // [START searchFiles]
+        $pageToken = null;
+        do {
+            $response = $driveService->files->listFiles(array(
+                'q' => "mimeType='image/jpeg'",
+                'spaces' => 'drive',
+                'pageToken' => $pageToken,
+                'fields' => 'nextPageToken, files(id, name)',
+            ));
+            foreach ($response->files as $file) {
+                printf("Found file: %s (%s)\n", $file->name, $file->id);
+            }
+            // [START_EXCLUDE silent]
+            array_push($files, $response->files);
+            // [END_EXCLUDE]
+    
+            $pageToken = $repsonse->pageToken;
+        } while ($pageToken != null);
+        // [END searchFiles]
+        return $files;
+    } catch(Exception $e) {
+       echo "Error Message: ".$e;
+    }
+   
 }
- // [END drive_searchFiles]
+ // [END searchFiles]
  searchFiles();
 ?>
